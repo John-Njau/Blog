f75206206741719226fddcbbe9a8eb6a4cef4865
john.njau <john.njoroge@student.moringaschool.com>
2022-05-15 13:30:24 +0300

    feature: mail configurations

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+
+class Config:
+    # SECRET_KEY=os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'tvybuinocdwmls'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:blog101db@localhost/blogdb'
+    DEBUG = True
+
+class ProdConfig(Config):
+    uri = os.getenv('DATABASE_URL')
+    if uri and uri.startswith('postgres://'):
+        uri = uri.replace('postgres://', 'postgresql://', 1)
+
+    SQLALCHEMY_DATABASE_URI = uri
+    
+
+
+class TestConfig(Config):
+    pass
+
+
+config_options = {
+    "development": DevConfig,
+    "production": ProdConfig,
+    "test": TestConfig,
+}
