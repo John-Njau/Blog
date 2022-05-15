f42dee3bdbc3081de1b5835a7b0f51d21ff3eb92
john.njau <john.njoroge@student.moringaschool.com>
2022-05-15 13:29:45 +0300

    feature: initializing and registering imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,39 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet, IMAGES
+from flask_mail import Mail 
 
-from app.main import main as main_blueprint
+from config import config_options
+
+# extension initialization
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos', IMAGES)
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
 
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
 
+    app.config.from_object(config_options[config_name])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    # app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///blogdb'
+
+    # instance initializations
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from app.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from app.auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
     return app
-
-
