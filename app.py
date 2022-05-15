7c4c8e4a692692945b79879c3761cd915ad587d0
john.njau <john.njoroge@student.moringaschool.com>
2022-05-15 13:31:22 +0300

    chore:initializing and  registering migrations

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-from app import create_app
+from app import create_app,db
+from app.models import Users,Blog,Photos,Comments,Subscriptions,Likes
+from flask_script import Manager,Server
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=Users, Blog=Blog, Likes=Likes, Photos=Photos, Comments=Comments, Subscriptions=Subscriptions)
 
 if __name__ == '__main__':
-    create_app()
+    manager.run()
