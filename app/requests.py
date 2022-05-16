d661ee9e21c1c92cf7398facb03b155a9f01fd82
john.njau <john.njoroge@student.moringaschool.com>
2022-05-17 00:24:21 +0300

    feature: random quotes http request

---
+++ app/requests.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def randomquote():
+    quotes = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
+    if quotes.status_code == 200:
+        quote = quotes.json()
+        return quote
+
+# import urllib.request
+# import json
+# from .models import Quote
+#
+# def configure_request(app):
+#   global
