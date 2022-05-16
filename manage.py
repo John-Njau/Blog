467459f6a199327770670d84b1c1fe6d95cb72da
john.njau <john.njoroge@student.moringaschool.com>
2022-05-17 02:02:31 +0300

    chore: remove migrations

--- manage.py
+++ manage.py
@@ -1,24 +1,28 @@
-from app import create_app,db
-from app.models import User,Blog,Photos,Comment,Subscriptions,Likes
-from flask_script import Manager,Server
+from app import create_app, db
+from app.models import User, Blog, Photos, Comment, Subscriptions, Likes
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('production')
 
 manager = Manager(app)
-manager.add_command('server',Server)
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
+manager.add_command('server', Server)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Blog=Blog, Likes=Likes, Photos=Photos, Comments=Comment, Subscriptions=Subscriptions)
+    return dict(app=app, db=db, User=User, Blog=Blog, Likes=Likes, Photos=Photos, Comments=Comment,
+                Subscriptions=Subscriptions)
+
 
 @manager.command
 def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 if __name__ == '__main__':
     manager.run()
